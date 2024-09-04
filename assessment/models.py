5c2f4b5f57134782aad80960c71d3e490e9b13fe
Divyanshu Singh <161971281+osmium0106@users.noreply.github.com>
2024-09-04 18:11:51 +0530

    Add files via upload

---
+++ assessment/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from users.models import *
+
+# Create your models here.
+class Quiz(models.Model):
+    name=models.CharField(max_length=150)
+    topic=models.CharField(max_length=150)
+    grade=models.CharField(max_length=100, default=0)
+    no_of_question=models.IntegerField()
+    time=models.IntegerField(help_text="duration of the quiz in minutes")
+    required_score_to_pass=models.IntegerField(help_text="required score to pass in %")
+
+    def __str__(self):
+        return f"{self.name}-{self.topic}"
+    
+    def get_questions(self):
+        return self.question_set.all()
+    
+    class Meta:
+        verbose_name_plural='Quizes'
+
+class Question(models.Model):
+    text=models.CharField(max_length=500)
+    quiz=models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    created=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.text)
+    
+    def get_answers(self):
+        return self.answer_set.all()
+
+class Answer(models.Model):
+    text=models.CharField(max_length=500)
+    correct=models.BooleanField(default=False)
+    question=models.ForeignKey(Question, on_delete=models.CASCADE)
+    created=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"question-{self.question.text}, answer-{self.text}, correct-{self.correct}"
+    
+class Result(models.Model):
+    quiz=models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    score=models.FloatField()
+
+    def __str__(self):
+        return str(self.pk)
---
+++ assessment/views.py
@@ -0,0 +1,83 @@
+from django.shortcuts import render,redirect
+from django.views.generic import ListView
+from .models import *
+from django.http import JsonResponse
+import math
+# Create your views here.
+
+class QuizView(ListView):
+    model=Quiz
+    template_name='quizes/main.html'
+    
+    def get_queryset(self):
+        # Assuming you have a way to identify the logged-in student and their grade
+        user=self.request.user
+        student =user_profile_student.objects.get(user=user)
+        student_grade=student.grade
+
+        # Filter quizzes based on the student's grade
+        queryset = Quiz.objects.filter(grade=student_grade)
+        return queryset
+
+def quiz_view(request,pk):
+    quiz=Quiz.objects.get(pk=pk)
+    return render(request, 'quizes/quizview.html', {'quiz':quiz})
+
+def quiz_data_view(request,pk):
+    quiz=Quiz.objects.get(pk=pk)
+    question=[]
+    for q in quiz.get_questions():
+        answers=[]
+        for a in q.get_answers():
+            answers.append(a.text)
+        question.append({str(q):answers})
+    return JsonResponse({'data':question,
+                         'time':quiz.time})
+                         
+
+def quiz_data_save(request,pk):
+    if request.is_ajax():
+        questions=[]
+        data=request.POST
+        data_=dict(data.lists())
+        data_.pop('csrfmiddlewaretoken')
+
+        for k in data_.keys():
+            print('key', k)
+            question=Question.objects.get(text=k)
+            questions.append(question)
+
+        user=request.user
+        quiz=Quiz.objects.get(pk=pk)
+
+        score=0
+        multiplier=100/quiz.no_of_question
+        results=[]
+        correct_answer=None
+
+        for q in questions:
+            a_selected=request.POST.get(q.text)
+
+            if a_selected != "":
+                question_answers=Answer.objects.filter(question=q)
+                for a in question_answers:
+                    if a_selected == a.text:
+                        if a.correct:
+                            score +=1
+                            correct_answer=a.text
+                    else:
+                        if a.correct:
+                            correct_answer=a.text
+                results.append({str(q):{"correct_answer":correct_answer, "answered":a_selected}})
+            else:
+                results.append({str(q):'not answered'})
+        score_=score*multiplier
+        Result.objects.create(quiz=quiz,user=user,score=score_)
+
+        if score_ >= quiz.required_score_to_pass:
+            return JsonResponse({"passed":True, "score":score_, "results":results})
+        else:
+            return JsonResponse({"passed":False, "score":score_,"results":results})
+
+
+        
